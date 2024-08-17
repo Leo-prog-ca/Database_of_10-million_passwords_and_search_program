e3b5fdae9b678c2b1fe9480b0dc3483770f017a3
Leo <arazov.itit@gmail.com>
2024-08-17 21:48:38 +0500

    Поиск паролей.py

--- Поиск паролей.py
+++ Поиск паролей.py
@@ -1,3 +1,18 @@
-def divide(a, b):
-    return a / b
-print(divide(10, 0))
+def find_password_in_file(file_path, password_to_check):
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.strip() == password_to_check:
+                return True
+    return False
+
+def main():
+    file_path = 'passwords.txt'  # Можно при желании добавить свою базу паролей
+    password_to_check = input("Введите пароль для проверки: ")
+
+    if find_password_in_file(file_path, password_to_check):
+        print("Пароль найден в списке.")
+    else:
+        print("Пароль не найден в списке.")
+
+if __name__ == "__main__":
+    main()
